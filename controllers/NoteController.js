c3b7a2058c8eaa949595e2cf31ed806cdcdc272e
davdmoo <david.mulyawan97@gmail.com>
2021-12-14 14:12:51 +0700

    add create note feature

--- controllers/NoteController.js
+++ controllers/NoteController.js
@@ -2,10 +2,35 @@ const { Note } = require('../models');
 
 const getNotes = async (req, res, next) => {
     try {
-        
+        const notes = await Note.findAll();
+
+        res.status(200).json(notes);
     } catch (err) {
         next(err);
     }
 }
 
-module.exports = { getNotes }
+const postNote = async (req, res, next) => {
+    try {
+        const newNote = await Note.create({
+            title: req.body.title,
+            status: req.body.status,
+            content: req.body.content,
+            label: req.body.label,
+            UserId: req.user.id
+        });
+
+        res.status(201).json({
+            id: newNote.id,
+            title: newNote.title,
+            content: newNote.content,
+            status: newNote.status,
+            label: newNote.label,
+            UserId: newNote.UserId
+        })
+    } catch (err) {
+        next(err);
+    }
+}
+
+module.exports = { getNotes, postNote };
--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,15 @@ const errorHandler = require('../middlewares/errorHandler.js');
 const authentication = require('../middlewares/authentication.js');
 
 const UserController = require('../controllers/UserController.js');
+const NoteController = require('../controllers/NoteController.js');
 
 router.post('/register', UserController.register);
 router.post('/login', UserController.login);
 
 router.use(authentication);
 
-router.get('/boards', (req, res) => res.send(`Get boards`));
+router.get('/notes', NoteController.getNotes);
+router.post('/notes', NoteController.postNote);
 
 router.use(errorHandler);
 
