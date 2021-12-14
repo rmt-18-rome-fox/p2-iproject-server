2eb1b0489fd01215a8574e7b909189620748ea05
Whisnubudih <whisnubudih@gmail.com>
2021-12-14 16:57:40 +0700

    login customer

--- controllers/CustomerController.js
+++ controllers/CustomerController.js
@@ -20,4 +20,43 @@ const registerCustomer = async(req,res,next) => {
 
 }
 
-module.exports = {registerCustomer}
+const loginCustomer = async(req,res,next)=>{
+    try {
+        const {email,password} = req.body
+        if(!email){
+            throw {name: "Email is required"}
+        }
+        if(!password){
+            throw {name: "Password is required"}
+        }
+
+       const result = await User.findOne({
+           where:{email}
+       })
+       if(!result){
+        throw {name: "Invalid email/password"}
+       }
+
+       const isPassword = bcrypt.compareSync(password, result.password)
+       if(!isPassword){
+        throw {name: "Invalid email/password"}
+       }
+       const payload ={
+           id:result.id,
+           email:result.email
+       }
+       const user ={
+        id : result.id,
+        email: result.email,
+        role: result.role
+    }
+
+       const access_token = signToken(payload)
+       res.status(200).json({access_token: access_token,user})
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports = {registerCustomer,loginCustomer}
