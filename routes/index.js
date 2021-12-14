e8dac8e751419c7ef7f336cce03ae1b1dc0e993b
naufaladitya04 <nar04012000@gmail.com>
2021-12-15 02:15:29 +0700

    added post router

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 const userRouter = require("../routes/userRouter");
 const errorHandler = require("../middlewares/errorHandler");
 const authentication = require("../middlewares/authentication");
+const postRouter = require("../routes/postrouter");
 
 router.use("/users", userRouter);
 router.use(authentication);
+router.use("/posts", postRouter);
 router.use(errorHandler);
 
 module.exports = router;
---
+++ routes/postrouter.js
@@ -0,0 +1,4 @@
+const router = require("express").Router();
+const postController = require("../controllers/postController");
+
+module.exports = router;
