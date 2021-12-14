84d6de188a63dfbf9924340413f05ea858405ac4
naufaladitya04 <nar04012000@gmail.com>
2021-12-14 16:05:57 +0700

    added feature login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
 const { User } = require("../models");
+const { sign } = require("../helpers/jwt");
+const { compare } = require("../helpers/bcryptjs");
 
 class UserController {
   static async register(req, res, next) {
@@ -21,6 +23,34 @@ class UserController {
       });
     } catch (err) {
       console.log(err);
+      next(err);
+    }
+  }
+  static async login(req, res, next) {
+    try {
+      const { email, password } = req.body;
+
+      if (!email) {
+        throw { name: "Bad Request", message: "Email is required" };
+      }
+      if (!password) {
+        throw { name: "Bad Request", message: "Password is required" };
+      }
+      const findUser = await User.findOne({ where: { email } });
+      if (!findUser) {
+        throw { name: "Unauthorized", message: "Invalid email/password" };
+      }
+      const isValidPassword = await compare(password, findUser.password);
+      if (!isValidPassword) {
+        throw { name: "Unauthorized", message: "Invalid email/password" };
+      }
+      const payload = {
+        id: findUser.id,
+        email: findUser.email,
+      };
+      const access_token = sign(payload);
+      res.status(200).json(access_token);
+    } catch (err) {
       next(err);
     }
   }
--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const SECRET_KEY = process.env.SECRET_KEY;
+const SECRET_KEY = "wowwow";
 
 function sign(payload) {
   const token = jwt.sign(payload, SECRET_KEY);
