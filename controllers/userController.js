8b62b2875626c4fe0e2a5997e9c0e23f6dec12c2
naufaladitya04 <nar04012000@gmail.com>
2021-12-14 15:31:53 +0700

    added feature register

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,29 @@
-class UserController {}
+const { User } = require("../models");
+
+class UserController {
+  static async register(req, res, next) {
+    try {
+      const { name, email, password, phoneNumber, address } = req.body;
+      const newUser = { name, email, password, phoneNumber, address };
+
+      if (!email) {
+        throw { name: "Bad Request", message: "Email is required" };
+      }
+
+      if (!password) {
+        throw { name: "Bad Request", message: "Password is required" };
+      }
+      const user = await User.create(newUser);
+      //   console.log(user);
+      res.status(201).json({
+        id: user.id,
+        email: user.email,
+      });
+    } catch (err) {
+      console.log(err);
+      next(err);
+    }
+  }
+}
 
 module.exports = UserController;
