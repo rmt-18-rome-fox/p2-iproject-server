bb29adb919ea2d778b5769418c4761fd4f65bd03
naufaladitya04 <nar04012000@gmail.com>
2021-12-14 15:02:56 +0700

    setup controller,index router,helper

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const router = require("./routes");
+const cors = require("cors");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
