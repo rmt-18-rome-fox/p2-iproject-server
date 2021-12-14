697e18acc6e4701c22dd5a8f20b80bdc20b8b00d
rizkybor <rizkyak994@gmail.com>
2021-12-14 13:12:46 +0700

    create model bookmark and user

---
+++ migrations/20211214061151-create-bookmark.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Bookmarks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name_club: {
+        type: Sequelize.STRING
+      },
+      fixture: {
+        type: Sequelize.DATE
+      },
+      league: {
+        type: Sequelize.STRING
+      },
+      venue: {
+        type: Sequelize.STRING
+      },
+      UserId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Bookmarks');
+  }
+};
