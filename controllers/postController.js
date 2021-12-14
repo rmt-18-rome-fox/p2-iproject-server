22ab1d841cfd0e3aa530e1aa2543ad5c5e3f39f1
naufaladitya04 <nar04012000@gmail.com>
2021-12-15 04:08:19 +0700

    get Posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,2 +1,23 @@
-class PostController {}
+const unsplash = require("../APIs/unsplash");
+const axios = require("axios");
+const { Post } = require("../models");
+const { toJSON, parse, stringify } = require("flatted");
+
+class PostController {
+  static async getPosts(req, res, next) {
+    try {
+      const resp = await axios.get("https://api.unsplash.com/photos/?client_id=r1SefPkNwhnq5e0uDLUG4jZ67qS6BRkOMi_Qy_Y4loU");
+
+      const postUsers = await Post.findAll();
+      //   console.log(toJSON(resp));
+      res.status(200).json({
+        result: resp.data,
+        postUsers,
+      });
+    } catch (err) {
+      console.log(err);
+      next(err);
+    }
+  }
+}
 module.exports = PostController;
