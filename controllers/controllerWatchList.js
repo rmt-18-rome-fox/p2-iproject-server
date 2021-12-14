4dea2839e876977d4a8a775b3ab131d97e1140e4
tondikii <tondikigg@gmail.com>
2021-12-15 03:46:33 +0700

    finishing get watchlists, add feature delete watchlists, and fix errorhandlers

--- controllers/controllerWatchList.js
+++ controllers/controllerWatchList.js
@@ -4,6 +4,8 @@ const postWatchList = async (req, res, next) => {
   try {
     const UserId = req.user.id
     const {priority, JikanAnimeId} = req.body
+    const isExists = await WatchList.findOne({where: {UserId, JikanAnimeId}})
+    if(isExists) throw {name: "This Anime Has Been Added To Your WatchList"}
     const watchList = await WatchList.create({
       UserId,
       JikanAnimeId,
@@ -12,7 +14,7 @@ const postWatchList = async (req, res, next) => {
     })
     res.status(201).json(watchList)
   } catch (err) {
-    console.log({err});
+    next(err)
   }
 }
 
@@ -23,8 +25,39 @@ const patchStatusWatchList = async (req, res, next) => {
     const watchList = await WatchList.update({status}, {where: {JikanAnimeId}})
     res.status(201).json({status})
   } catch (err) {
-    console.log({err});
+    next(err)
   }
 }
 
-module.exports = {postWatchList, patchStatusWatchList}
+const getWatchLists = async (req, res, next) => {
+  try {
+    const UserId = req.user.id
+    const watchlists = await WatchList.findAll({
+      where: {
+        UserId
+      },
+      include: {
+        model: User,
+        attributes: {
+          exclude: ["password"]
+        }
+      }
+    })
+    res.status(200).json(watchlists)
+  } catch (err) {
+    next(err)
+  }
+}
+
+const deleteWatchList = async (req, res, next) => {
+  try {
+    const UserId = req.user.id
+    const {JikanAnimeId} = req.params
+    const destroy = WatchList.destroy({where: {UserId, JikanAnimeId}})
+    res.status(200).json("Succes Delete WatchList")
+  } catch (err) {
+    next(err)
+  }
+}
+
+module.exports = {postWatchList, patchStatusWatchList, getWatchLists, deleteWatchList}
