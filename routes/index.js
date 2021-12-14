fe956722e71b535ee5e38bb9214a68c306ce02c4
naufaladitya04 <nar04012000@gmail.com>
2021-12-15 02:05:43 +0700

    implentation on router

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const userRouter = require("../routes/userRouter");
 const errorHandler = require("../middlewares/errorHandler");
+const authentication = require("../middlewares/authentication");
 
 router.use("/users", userRouter);
+router.use(authentication);
 router.use(errorHandler);
 
 module.exports = router;
