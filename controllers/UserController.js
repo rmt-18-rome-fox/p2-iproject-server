f3b1ffd1379bac6d38c1fcab03ead927fb144c7c
davdmoo <david.mulyawan97@gmail.com>
2021-12-14 12:44:21 +0700

    register and login feature done

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,4 +1,7 @@
 const { User } = require('../models');
+const secretKey = process.env.SECRETKEY;
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const register = async (req, res, next) => {
     try {
@@ -16,7 +19,24 @@ const register = async (req, res, next) => {
 
 const login = async (req, res, next) => {
     try {
-        
+        const { email, password } = req.body;
+        if (!email) throw { name: 'EmailRequired' };
+        else if (!password) throw { name: 'PassRequired' };
+
+        const user = await User.findOne({ where: { email } });
+        if (!user) throw { name: 'Unauthorized' };
+
+        const passValidation = bcrypt.compareSync(password, user.password);
+        if (!passValidation) throw { name: 'Unauthorized' };
+
+        const payload = {
+            id: user.id,
+            email: user.email
+        }
+
+        const access_token = jwt.sign(payload, secretKey);
+
+        res.status(200).json({ access_token });
     } catch (err) {
         next(err);
     }
--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -3,9 +3,15 @@ const errorHandler = (err, req, res, next) => {
     console.log(err, '<<< ERROR HANDLER');
 
     switch (err.name) {
-        // case value:
-            
-        //     break;
+        case 'EmailRequired':
+            res.status(400).json({ message: 'Email is required' });
+            break;
+        case 'PassRequired':
+            res.status(400).json({ message: 'Password is required' });
+            break;
+        case 'Unauthorized':
+            res.status(401).json({ message: 'Invalid email/password' });
+            break;
         case 'SequelizeValidationError':
             res.status(400).json({ message: err.errors[0].message });
             break;
