91e12c8972de4cc879d092843f0ac93581c2c604
ihzaw <ihzanantama@gmail.com>
2021-12-15 18:52:36 +0800

    server complete

--- app.js
+++ app.js
@@ -3,16 +3,18 @@ const app =  express()
 const port = 3000
 const cors = require('cors')
 const { errHandler } = require('./middlewares/errHandler')
+const { auth } = require('./helpers/auth')
 const Controller = require('./controller')
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-// app.post('/register', Controller.register)
-// app.post('/login', Controller.login)
-// app.post('/:userId/history', Controller.getHistory)
-// app.get('/:userId/history', Controller.getHistory)
+app.post('/register', Controller.register)
+app.post('/login', Controller.login)
+app.use(auth)
+app.post('/:UserId/history', Controller.postHistory)
+app.get('/:UserId/history', Controller.getHistory)
 
 app.use(errHandler)
 app.listen(port, () => {
--- controller.js
+++ controller.js
@@ -1,21 +1,81 @@
+const { User, History } =  require('./models')
+const { signToken, verifyToken } = require('./helpers/jwt')
+const bcrypt = require('bcrypt')
+const { sign } = require('jsonwebtoken')
+
 class Controller {
-    static async login (req, res, next) {
+    static async register (req, res, next) {
         try {
-            
+            const { name, email, password } = req.body
+            console.log(req.body)
+            const found = await User.findOne({
+                where: {
+                    email
+                }
+            })
+            if (found) {
+                throw {name: 'accountExisted'}
+            } else {
+                const response = await User.create({ name, email, password })
+                const payload = {
+                    id: response.id,
+                    name: response.name
+                }
+                res.status(201).json(payload)
+            }
         } catch (err) {
             next(err)
         }
     }
-    static async register (req, res, next) {
+    static async login (req, res, next) {
         try {
-            
+            const { email, password } = req.body
+            if(!email) throw {name: 'noEmail'}
+            if(!password) throw {name: 'noPassword'}
+
+            const response = await User.findOne({ where: {email} })
+            if (!response) throw {name: 'invalidLogin'}
+
+            if(!bcrypt.compare(password, response.password)) throw {name: 'invalidLogin'}
+
+            const payload = {
+                id: response.id,
+                name: response.name,
+                email: response.email
+            }
+            const access_token = signToken(payload)
+            res.status(200).json({ access_token })
+        } catch (err) {
+            next(err)
+        }
+    }
+    static async postHistory (req, res, next) {
+        try {
+            const { distance, carbonEmitted, origin, destination } = req.body
+            const { UserId } = req.params
+            const description = `shipment from ${origin} to ${destination}`
+            console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", distance, carbonEmitted, origin, destination, UserId)
+            const response =  await History.create({ distance, carbonEmitted, description, UserId })
+            if(!response) {
+                console.log('History not saved', err)
+            }
+            res.status(200).json({message: 'Saved History'})
         } catch (err) {
             next(err)
         }
     }
     static async getHistory (req, res, next) {
         try {
-            
+            const { UserId } = req.params
+            const response = await History.findAll({
+                where: {
+                    UserId
+                },
+                order: [
+                    ['createdAt', 'DESC']
+                ]
+            })
+            res.status(200).json(response)
         } catch (err) {
             next(err)
         }
