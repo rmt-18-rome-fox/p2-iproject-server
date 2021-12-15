88f7b015d5f7cf6f8d705e551502abb015a77ad7
michaelwijanto <michael.wijanto@gmail.com>
2021-12-15 15:31:15 +0700

    create endpoint to call dnd api

---
+++ controllers/APIController.js
@@ -0,0 +1,39 @@
+const axios = require("axios");
+const baseUrl = "https://www.dnd5eapi.co/api";
+class APIController {
+  static async getClasses(req, res, next) {
+    try {
+      const { data } = await axios({
+        method: "get",
+        url: `${baseUrl}/classes`,
+      });
+      res.status(200).json(data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  static async getRaces(req, res, next) {
+    try {
+      const { data } = await axios({
+        method: "get",
+        url: `${baseUrl}/races`,
+      });
+      res.status(200).json(data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  static async getSpells(req, res, next) {
+    try {
+      const { data } = await axios({
+        method: "get",
+        url: `${baseUrl}/spells`,
+      });
+      res.status(200).json(data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+module.exports = APIController;
