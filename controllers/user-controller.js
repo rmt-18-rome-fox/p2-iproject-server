604d4cc32fa64ddc47bf7e4d47b089f689e334cb
hwh96 <hwinuratama@gmail.com>
2021-12-16 01:21:38 +0700

    added feature socket

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -41,13 +41,17 @@ class Controller {
             if(!isValid) {
                 throw {name: 'UserNotFound'};
             }
-
+            const user = {
+                id: findUser.id,
+                username: findUser.username,
+                email: findUser.email
+            }
             const payload = {
                 id: findUser.id,
                 username: findUser.username,
             }
             const token = signPayload(payload, isValid);
-            res.status(200).json({access_token: token, dataUser: payload});
+            res.status(200).json({access_token: token, dataUser: user});
 
         } catch (err) {
             next(err)
