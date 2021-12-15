c805bbaaca061472ab162e493493c64b0cee2e5c
hwh96 <hwinuratama@gmail.com>
2021-12-16 03:28:50 +0700

    added news api

---
+++ apis/newsApi.js
@@ -0,0 +1,9 @@
+const axios = require("axios")
+
+
+const newsAxios = axios.create({
+    baseURL: 'http://api.mediastack.com/v1/'
+})
+
+
+module.exports = newsAxios;
--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,6 @@ const user = require('./user');
 const fans = require('./fans');
 const post = require('./post');
 const like = require('./like');
-<<<<<<< HEAD
-const Controller = require('../controllers/user-controller')
-const ballController = require('../controllers/ball-controller')
-const {authentication} = require('../middlewares/middelware')
-
-router.use('/users', user);
-router.use('/fans', fans);
-router.post('/login', Controller.login);
-=======
 const Controller = require('../controllers/user-controller');
 const ballController = require('../controllers/ball-controller');
 const {authentication} = require('../middlewares/middelware');
@@ -19,12 +10,11 @@ const {authentication} = require('../middlewares/middelware');
 router.use('/users', user);
 router.use('/fans', fans);
 router.post('/login', Controller.login)
->>>>>>> 604d4cc32fa64ddc47bf7e4d47b089f689e334cb
 
 router.use(authentication);
 
 router.get('/standing', ballController.getStandings)
-router.get('/club-info', ballController.getClubs)
+router.get('/news', ballController.getNews)
 router.use('/post', post);
 router.use('/likes', like)
 
