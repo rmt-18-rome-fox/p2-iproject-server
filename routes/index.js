8511fba254e2c4a5319e25dd75427096e1cb6fbe
hwh96 <hwinuratama@gmail.com>
2021-12-16 06:47:16 +0700

    add google login

--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,19 @@ const post = require('./post');
 const like = require('./like');
 const Controller = require('../controllers/user-controller');
 const ballController = require('../controllers/ball-controller');
+const {authGoogle} = require('../controllers/auth-controller');
 const {authentication} = require('../middlewares/middelware');
 
 router.use('/users', user);
 router.use('/fans', fans);
-router.post('/login', Controller.login)
+router.post('/login', Controller.login);
+router.post('/authGoogle', authGoogle);
 
 router.use(authentication);
 
-router.get('/standing', ballController.getStandings)
-router.get('/news', ballController.getNews)
+router.get('/standing', ballController.getStandings);
+router.get('/news', ballController.getNews);
 router.use('/post', post);
-router.use('/likes', like)
+router.use('/likes', like);
 
 module.exports = router;
