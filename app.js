c9f06a0f69c265a7011087a9356c7daf00bc7d48
hwh96 <hwinuratama@gmail.com>
2021-12-15 21:39:04 +0700

    init socket

--- app.js
+++ app.js
@@ -5,6 +5,34 @@ const port = 3000;
 const cors = require('cors');
 const router = require('./routes')
 const {errHandler} = require('./middlewares/errorHandler');
+const {createServer} = require('http');
+const {Server} = require('socket.io');
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+  }
+});
+
+let userData = []
+io.on('connection', (socket) => {
+  console.log('User connected', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  })
+
+  socket.on('user', (value) => {
+    userData.push({
+      username: value,
+      status: 'Online'
+    })
+    socket.emit('customEventServer', 'server exchange');
+    console.log(userData);
+  })
+
+})
 
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
@@ -13,6 +41,9 @@ app.use(router)
 
 app.use(errHandler)
 
-app.listen(port, () => {
-  console.log(`Web app listening at http://localhost:${port}`)
+// app.listen(port, () => {
+//   console.log(`Web app listening at http://localhost:${port}`)
+// })
+httpServer.listen(port, () => {
+  console.log(`Web app listening at http://localhost:${port}`);
 })
