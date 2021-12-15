8c25c22bfeda9120578f9ca8dec2597ac7c875cf
azmifitra <azmi801@gmail.com>
2021-12-15 19:43:41 +0700

    fixed order detail and some midtrans issues

--- controllers/controllerMidtrans.js
+++ controllers/controllerMidtrans.js
@@ -12,20 +12,40 @@ const postMidtrans = async (req, res, next) => {
   try {
     const UserId = req.user.id;
     const idOrderDetail = req.params.orderid;
+    // console.log(UserId);
 
-    const findOrderDetail = await OrderDetail.findOne({
-      where: { UserId: UserId, id: idOrderDetail },
+    const findOrderDetail = await OrderDetail.findAll({
+      where: { UserId: UserId, orderStatus: 'pending' },
       include: { model: CoffeePowder },
     });
-
     if (!findOrderDetail) {
       throw { name: 'notFound' };
     }
 
+    const findOneOrderDetail = await OrderDetail.findOne({
+      where: { UserId: UserId, orderStatus: 'pending' },
+      include: { model: CoffeePowder },
+    });
+
+    // console.log(findOneOrderDetail.orderId, '<<<<<<<<<<<<<<, INI FIND ONE');
+
+    // =================== HITUNG TOTAL PRICE CART ===================
+    let totalPrice = 0;
+
+    const tampung = findOrderDetail.forEach((el) => {
+      // console.log(el.CoffeePowder.price * el.quantity);
+      totalPrice += el.CoffeePowder.price * el.quantity;
+    });
+
+    // GENERATE ORDER_ID
+    let cartOrderId = 'COFFPOW-ORDER-' + findOneOrderDetail.orderId;
+
+    console.log(totalPrice, cartOrderId, '<<<<<<<<<<<<<<<<< INI TOTAL PRICE X ORDER_ID');
+
     // console.log(findOrderDetail.CoffeePowder.price);
     // console.log(findOrderDetail.quantity);
 
-    const totalPrice = findOrderDetail.CoffeePowder.price * findOrderDetail.quantity;
+    // const totalPrice = findOrderDetail.CoffeePowder.price * findOrderDetail.quantity;
 
     const response = await axios({
       url: `${sandboxBaseUrl}/snap/v1/transactions`,
@@ -37,7 +57,7 @@ const postMidtrans = async (req, res, next) => {
       },
       data: {
         transaction_details: {
-          order_id: `COFFPOW-ORDER-${findOrderDetail.orderId}`,
+          order_id: `${cartOrderId}`,
           gross_amount: totalPrice,
         },
       },
@@ -46,29 +66,62 @@ const postMidtrans = async (req, res, next) => {
     // console.log(response.data);
     const result = response.data;
 
-    res.status(200).json(result);
+    const responseStatus2 = await axios({
+      url: `${apiSandboxBaseUrl}/v2/${cartOrderId}/status`,
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: `Basic ${authString}`,
+      },
+    });
+
+    let tampungStatus2 = {
+      transaction_status: responseStatus2.data.transaction_status,
+      fraud_status: responseStatus2.data.fraud_status,
+      status_message: responseStatus2.data.status_message,
+    };
+    // console.log(tampungStatus);
+
+    console.log(responseStatus2.data);
+    // console.log(tampungStatus);
+
+    // res.status(200).json(tampungStatus);
+
+    res.status(200).json({ result });
   } catch (err) {
     console.log(err.response);
     console.log('EROR ON MIDTRANS');
-    // next(err);
+    next(err);
   }
 };
 
 const checkStatusPayment = async (req, res, next) => {
   try {
-    const order_Id = req.params.orderid;
+    // const order_Id = req.params.orderid;
+    const UserId = req.user.id;
     // console.log(order_Id);
 
-    const findOrderDetail = await OrderDetail.findOne({
-      where: { orderId: order_Id },
+    // const findOrderDetail = await OrderDetail.findOne({
+    //   where: { orderId: order_Id },
+    // });
+
+    // if (!findOrderDetail) {
+    //   throw { name: 'notFound' };
+    // }
+    const findOneOrderDetail = await OrderDetail.findOne({
+      where: { UserId: UserId, orderStatus: 'pending' },
+      include: { model: CoffeePowder },
     });
 
-    if (!findOrderDetail) {
+    if (!findOneOrderDetail) {
       throw { name: 'notFound' };
     }
 
+    let cartOrderId = 'COFFPOW-ORDER-' + findOneOrderDetail.orderId;
+
     const response = await axios({
-      url: `${apiSandboxBaseUrl}/v2/COFFPOW-ORDER-${order_Id}/status`,
+      url: `${apiSandboxBaseUrl}/v2/${cartOrderId}/status`,
       method: 'get',
       headers: {
         'Content-Type': 'application/json',
@@ -85,6 +138,10 @@ const checkStatusPayment = async (req, res, next) => {
     // console.log(tampungStatus);
 
     console.log(response.data);
+
+    if (tampungStatus.transaction_status == 'capture') {
+      await OrderDetail.update({ orderStatus: 'complete' }, { where: { orderStatus: 'pending' } });
+    }
 
     res.status(200).json(tampungStatus);
   } catch (err) {
