7b9663a830446ed3ca1eb4beea2eaa3297faa666
syswyndar <sysswyndar@gmail.com>
2021-12-17 03:18:56 +0700

    add new routes  for get user detail

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 router.use(authentication)
 
+router.get('/users', UserController.getUser)
 router.get('/juzz', DataController.getAllJuzz)
 router.get('/juzz/:juzsNumber', DataController.getAyahByJuzs)
 router.get('/chapters', DataController.getAllChapter)
