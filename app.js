cde2e636daab307049dd5f513c19bcd42eb0ec9b
muhano <geri.muhano@gmail.com>
2021-12-16 07:18:36 +0700

    heroku1

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const errorHandler = require('./middlewares/errorHandler')
 // const { Server } = require("socket.io");
 // const io = new Server(server);
 
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.json()) // for parsing application/json
