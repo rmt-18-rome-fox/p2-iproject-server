6f21e5ad1b930b918f97804537eecd2d6daf7a05
Tondiki Andika <tondikiandika@Tondikis-MacBook-Air.local>
2022-11-06 01:47:59 +0700

    update version jikan api

--- controllers/ControllerJikan.js
+++ controllers/ControllerJikan.js
@@ -3,7 +3,8 @@ const axios = require('axios');
 const getAnime = async (req, res, next) => {
   try {
     const page = req.query.page || 1;
-    const anime = await axios.get(`https://api.jikan.moe/v3/top/anime/${page}`)
+    const limit = req.query.limit || 24;
+    const anime = await axios.get(`https://api.jikan.moe/v4/top/anime?page=${page}&limit=${limit}`)
     res.status(200).json(anime.data)
   } catch (err) {
     next(err)
@@ -13,7 +14,7 @@ const getAnime = async (req, res, next) => {
 const getAnimeDetail = async (req, res, next) => {
   try {
     const jikanAnimeId = req.params.jikanAnimeId;
-    const animeDetail = await axios.get(`https://api.jikan.moe/v3/anime/${jikanAnimeId}`)
+    const animeDetail = await axios.get(`https://api.jikan.moe/v4/anime/${jikanAnimeId}`)
     res.status(200).json(animeDetail.data)
   } catch (err) {
     next(err);
